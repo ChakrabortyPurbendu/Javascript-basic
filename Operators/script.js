0d0503efcfbce0acfbbcf1a2da6190edfeb3b206
chakrabortyPurbendu <21purbenduchakraborty@gmail.com>
2025-06-20 21:18:59 +0530

    Variables and datatypes discussed

---
+++ Operators/script.js
@@ -0,0 +1,7 @@
+console.log("Today we are gona explore operators and conditional statements in full details...")
+
+/*
+Operators and conditions
+
+
+*/
---
+++ script.js
@@ -0,0 +1,39 @@
+console.log("Namasthey dhuniya hello ji sare welcome to code help...")
+
+
+/*    
+
+Variables and datatypes 
+variables are named memory locations that are used to define the containers to store the values
+
+let age= 25
+
+var let const 
+
+Number 1 ,2 3 4 4.43 5.78 3.14 
+String "love" "babbar" character 'a'-'z'  'A'-'Z' 
+undefined 
+null
+bigint symbol
+
+
+
+
+
+*/
+// let age=25;
+// console.log(age);
+// let name="Babbar";
+// console.log(name);
+
+let a=10;
+a="babbar love"
+console.log(a);
+console.log(typeof(a));
+
+let marks=null;
+console.log(marks);
+
+let name;
+console.log(name);
+
